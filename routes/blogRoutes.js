b344487bcdd6aae83d76f8a780afec0d4bab0c57
PRoy <sam469roger@gmail.com>
2023-07-14 14:50:05 +0530

    redis - promisifying

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,17 +3,30 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const { promisify } = require('util');
+    client.get = promisify(client.get);
+
+    // do we have any cachd data in redis related to this query
+    const cachedBlogs = await client.get(req.user.id);
+
+    // if yes then respond to the request right away and return
+
+    // if no we need to respond to request and update our cache to store the data
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
@@ -25,7 +38,7 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
