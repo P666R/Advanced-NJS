bac2405b326c3f14fc09c129798a03e5df3edc45
PRoy <sam469roger@gmail.com>
2023-07-16 07:28:46 +0530

    nested hashes

--- services/cache.js
+++ services/cache.js
@@ -4,11 +4,12 @@ const { promisify } = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = promisify(client.get);
+client.hget = promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
   return this;
 };
 
@@ -25,7 +26,7 @@ mongoose.Query.prototype.exec = async function () {
 
   // see if we have a value for key in redis
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // if yes return it
   if (cacheValue) {
@@ -39,6 +40,8 @@ mongoose.Query.prototype.exec = async function () {
   // otherwise run the query and store the result in redis
 
   const result = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(result));
+
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
+
   return result;
 };
